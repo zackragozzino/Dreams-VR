df33f0072e23f644e6e36019c98fe9dccba13b10
Zack Ragozzino <zackragozzino@gmail.com>
2018-01-29 18:45:52 -0800

    Initial portal test
    Basic portal working in a door frame with teleport

---
+++ Dreams VR/Assets/Scripts/PortalCamera.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalCamera : MonoBehaviour {
+	public Transform playerCamera;
+	public Transform portal;
+	public Transform otherPortal;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		//Moves the camera to match movement of the player (with the initial offset)
+		Vector3 playerOffsetFromPortal = playerCamera.position - otherPortal.position;
+		transform.position = portal.position + playerOffsetFromPortal;
+
+		//Rotate the camera to match the direction the player is looking in
+		float angularDifferenceBetweenPortalRotations = Quaternion.Angle (portal.rotation, otherPortal.rotation);
+
+		//ew...
+		Quaternion portalRotationalDifference = Quaternion.AngleAxis (angularDifferenceBetweenPortalRotations, Vector3.up);
+		Vector3 newCameraDirection = portalRotationalDifference * playerCamera.forward;
+		transform.rotation = Quaternion.LookRotation (newCameraDirection, Vector3.up);
+		
+	}
+}
---
+++ Dreams VR/Assets/Scripts/PortalTextureSetup.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalTextureSetup : MonoBehaviour {
+
+	public Camera cameraA;
+	public Camera cameraB;
+
+	public Material cameraMatA;
+	public Material cameraMatB;
+
+	void Start () {
+		
+		/*
+		 * Basically sets up the initial resolution of the portals
+		 * so that they match the resolution of the computer screen
+		 */
+
+		if (cameraA.targetTexture != null)
+		{
+			cameraA.targetTexture.Release();
+		}
+		cameraA.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
+		cameraMatA.mainTexture = cameraA.targetTexture;
+
+		if (cameraB.targetTexture != null)
+		{
+			cameraB.targetTexture.Release();
+		}
+		cameraB.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
+		cameraMatB.mainTexture = cameraB.targetTexture;
+	}
+}
