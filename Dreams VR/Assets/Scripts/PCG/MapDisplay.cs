a84bbd8a13c810bac34b8d7d9caf1037e11e1506
Zack Ragozzino <zackragozzino@gmail.com>
2018-02-05 01:42:28 -0800

    Early terrain testing
    Simple 2D and 3D terrain generation

---
+++ Dreams VR/Assets/Scripts/PCG/MapDisplay.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapDisplay : MonoBehaviour {
+
+	public Renderer textureRenderer;
+
+	public void DrawNoiseMap(float [,] noiseMap){
+		int width = noiseMap.GetLength (0);
+		int height = noiseMap.GetLength (1);
+
+		Texture2D texture = new Texture2D (width, height);
+
+		/*Calculate all color values before applying them to the texture.
+		* This is considered faster than applying them after each calculation.
+		*/
+		Color[] colorMap = new Color[width * height];
+		for (int y = 0; y < height; y++) {
+			for (int x = 0; x < width; x++) {
+				//Index of colormap relative to the index in the noise map 2D array
+				colorMap[y * width + x] = Color.Lerp(Color.black, Color.white, noiseMap[x, y]);
+			}
+		}
+		texture.SetPixels (colorMap);
+		texture.Apply ();
+
+		textureRenderer.sharedMaterial.mainTexture = texture;
+		textureRenderer.transform.localScale = new Vector3 (width, 1, height);
+	}
+}
---
+++ Dreams VR/Assets/Scripts/PCG/MapGeneratorEditor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor (typeof (MapGenerator))]
+public class MapGeneratorEditor : Editor {
+	/*
+	 * Applies the generated texture to the
+	 * target plane in the Unity editor mode.
+	 * This is just to make testing easier.
+	 */
+	public override void OnInspectorGUI(){
+		MapGenerator mapGen = (MapGenerator)target;
+
+		if (DrawDefaultInspector ()) {
+			if (mapGen.autoUpdate)
+				mapGen.GenerateMap ();
+		}
+
+		if (GUILayout.Button ("Generate")) {
+			mapGen.GenerateMap ();
+		}
+	}
+}
