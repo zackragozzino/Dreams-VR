2cbe1c89be6cb390a42b985817d4002d13b487f6
Paula Ledgerwood <paledger@gmail.com>
2018-02-16 08:07:54 -0800

    cubes will now move towards the player

--- Dreams VR/Assets/Scripts/PaulaScripts/CreepyCrawlers.cs
+++ Dreams VR/Assets/Scripts/PaulaScripts/CreepyCrawlers.cs
@@ -6,39 +6,55 @@ public class CreepyCrawlers : MonoBehaviour {
 
     public GameObject crawler;
     public GameObject center;
-    public int numStartCrawlers;
-    public int maxCrawlers;
-    public float radius;
-    public float speed;
-    public List<GameObject> crawlers;
-
-    public CreepyCrawlers () {
-        this.numStartCrawlers = 0;
-        this.maxCrawlers = 10;
-        this.radius = 10.0f; 
-        this.speed = 0.4f;
-    }
+    public int numStartCrawlers = 0;
+    public int maxCrawlers = 10;
+    public float radius = 25.0f;
+    public float speed = 1.0f;
+    public float restTime = 3;
+    private List<GameObject> crawlers;
+    private float closest = 1;
+    private bool move = true;
 
     // Use this for initialization
     void Start () {
-
+        Quaternion zeroQuaternion = new Quaternion(0.0f, 0.0f, 0.0f, 0.0f);
+        float crawlerAngle = 0;
+        float angleIncrement = 2 * Mathf.PI / this.maxCrawlers;
         for (int i = 0; i < this.maxCrawlers; i++) {
-            print("Instantiate!");
-            crawlers.Add((GameObject)Instantiate(this.crawler, this.crawler.transform,instantiateInWorldSpace:true));
+            float x = Mathf.Sin(crawlerAngle) * this.radius;
+            float z = Mathf.Cos(crawlerAngle) * this.radius;
+            Vector3 newPos = this.center.transform.position + new Vector3(x, 0.0f, z);
+            print("Instantiate");
+            print(i);
+            print(newPos);
+            crawlers.Add((GameObject)Instantiate(this.crawler, newPos, zeroQuaternion));
+            crawlerAngle += angleIncrement;
         }
+        StartCoroutine(Wait(5.0f));
     }
     
     // Update is called once per frame
-    void Update () {      
-        // foreach (GameObject c in crawlers) {
-        //     Vector3 newDir = this.center.transform.position - c.transform.position;
-        //     if (newDir.x > 3 && newDir.y > 3) {
-        //         c.transform.Translate(newDir * this.speed);
-        //     }
-        // }
+    void Update () {   
+        if (this.move == true) {
+            foreach (GameObject c in crawlers) {
+                Vector3 diffVector = this.center.transform.position - c.transform.position;
+                if (diffVector.magnitude >= closest) {
+                    print(diffVector.magnitude);
+                    print("Closest " + closest);
+                    print(diffVector);
+                    Vector3 translate = diffVector * this.speed * Time.deltaTime;
+                    c.transform.Translate(translate);
+                    print("Hi there!");
+                    print(c.transform.position);
+                }
+            }
+            StartCoroutine(Wait(3.0f));
+        }
     }
 
-    IEnumerator wait(int timeWait) {
-        yield return new WaitForSeconds(timeWait);
+    private IEnumerator Wait(float timeWait) {
+        this.move = false;
+        yield return new WaitForSecondsRealtime(timeWait);
+        this.move = true;
     }
 }
