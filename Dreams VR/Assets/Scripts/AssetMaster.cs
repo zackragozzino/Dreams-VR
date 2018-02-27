b5e0f5454b292e4b744b0e37ec9315258431f439
Zack Ragozzino <zackragozzino@gmail.com>
2018-02-27 00:38:47 -0800

    More proc gen stuff

--- Dreams VR/Assets/Scripts/AssetMaster.cs
+++ Dreams VR/Assets/Scripts/AssetMaster.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class AssetMaster : MonoBehaviour {
 
 	public Object_Populator ObjectPopulator;
+	public bool generateAssets;
 	public GameObject[] forestAssets;
 	public GameObject[] urbanAssets;
+	public GameObject[] furnitureAssets;
+	public GameObject[] palmTreeAssets;
 }
--- Dreams VR/Assets/Scripts/ProcGen/Object_Populator.cs
+++ Dreams VR/Assets/Scripts/ProcGen/Object_Populator.cs
@@ -67,29 +67,33 @@ public class Object_Populator : MonoBehaviour {
 	public void Populate() {
 		int width = heightMap.values.GetLength (0);
 		int height = heightMap.values.GetLength (1);
+		AssetMaster assetMaster = transform.GetComponentInParent<AssetMaster> ();
 
-		GameObject[] assets = transform.GetComponentInParent<AssetMaster> ().forestAssets;
-		GameObject[] urbanAssets = transform.GetComponentInParent<AssetMaster> ().urbanAssets;
+		GameObject[] assets = assetMaster.urbanAssets;
+		GameObject[] urbanAssets = assetMaster.urbanAssets;
 
-		if (heightMapReceived) {
+		if (heightMapReceived && assetMaster.generateAssets) {
 			float[] noiseMap = new float[width * height];
 			for (int y = 0; y < height; y++) {
 				for (int x = 0; x < width; x++) {
 					if (heightMap.values [x, y] > 0.7f) {
 						GameObject asset = Instantiate (assets[Random.Range(0, assets.Length)], new Vector3 (this.transform.position.x + x - (width/2f), 0, this.transform.position.z + y - (height/2f)), Quaternion.identity, this.transform);
 						//GameObject asset = Instantiate (testMesh, new Vector3 (this.transform.position.x + x - (width/2f), 0, this.transform.position.z + y - (height/2f)), Quaternion.identity, this.transform);
-						asset.transform.localScale = asset.transform.localScale * 10 * Random.value;
+						//asset.transform.localScale = asset.transform.localScale * (10 * Random.value + 4);
+						asset.transform.localScale = asset.transform.localScale * 0.5f;
 					}
 
 					if(heightMap.values[x,y] < 0.0){
 						//GameObject asset = Instantiate (urbanAssets[Random.Range(0, urbanAssets.Length)], new Vector3 (this.transform.position.x + x - (width/2f), Random.Range(5,15), this.transform.position.z + y - (height/2f)), Quaternion.identity, this.transform);
 						GameObject asset = Instantiate (testMesh, new Vector3 (this.transform.position.x + x - (width/2f), Random.Range(20,100), this.transform.position.z + y - (height/2f)), Quaternion.identity, this.transform);
 
+						asset.transform.eulerAngles = new Vector3 (Random.Range (0, 360), Random.Range (0, 360), Random.Range (0, 360));
+
 						Vector3 dir = new Vector3 (20, 0, 0);
 						asset.AddComponent<Rotater> ().RotationPerSecond = dir;
 
 						asset.transform.localScale = asset.transform.localScale * 10 * Random.value;
-						asset.transform.eulerAngles = new Vector3 (Random.Range (0, 360), Random.Range (0, 360), Random.Range (0, 360));
+
 					}
 				}
 			}
