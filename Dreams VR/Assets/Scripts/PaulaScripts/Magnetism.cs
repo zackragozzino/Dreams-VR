8f3a13ce2c5f8027a63bea51580eab16c1a776ff
Paula Ledgerwood <paledger@gmail.com>
2018-03-20 18:21:36 -0700

    commented creepycrawler code

--- Dreams VR/Assets/Scripts/PaulaScripts/Magnetism.cs
+++ Dreams VR/Assets/Scripts/PaulaScripts/Magnetism.cs
@@ -4,28 +4,50 @@ using UnityEngine;
 
 public class Magnetism : MonoBehaviour {
 
-	public GameObject magnet;
+	public GameObject[] magnets;
 	public GameObject player;
-	public int effectiveRadius;
-	public float magneticStrength;
+	public int effectiveRadius = 10;
+	public float magneticStrength = 1;
 
 	// Use this for initialization
 	void Start () {
 		if (player == null) {
 			player = GameObject.FindGameObjectWithTag ("Player");
 		}
+      magnets = GameObject.FindGameObjectsWithTag("Magnet");
+      foreach (GameObject magnet in magnets) {
+         checkRigidbody(magnet);
+         checkCollider(magnet);
+      }
 	}
 	
+   void checkRigidbody(GameObject item) {
+      Rigidbody rigidbody = item.GetComponent(typeof(Rigidbody)) as Rigidbody;
+      if (!rigidbody) {
+         rigidbody = item.AddComponent(typeof(Rigidbody)) as Rigidbody;
+      }
+      rigidbody.constraints = RigidbodyConstraints.FreezePositionY | 
+         RigidbodyConstraints.FreezeRotationX |
+         RigidbodyConstraints.FreezeRotationZ;
+   }
+
+   void checkCollider(GameObject item){
+      Collider collider = item.GetComponent(typeof(Collider)) as Collider;
+      if (!collider) {
+         collider = item.AddComponent(typeof(BoxCollider)) as BoxCollider;
+      }
+   }
+
 	// Update is called once per frame
 	void Update () {
-		Vector3 difference = new Vector3(magnet.transform.position.y, 0.0f, magnet.transform.position.z) - new Vector3(player.transform.position.y, 0.0f, player.transform.position.z);
-		if (difference.magnitude <= this.effectiveRadius) {
-			Rigidbody rigidbody = magnet.GetComponent(typeof(Rigidbody)) as Rigidbody;
-			if (!rigidbody) {
-				rigidbody = magnet.AddComponent(typeof(Rigidbody)) as Rigidbody;
-			}
-			rigidbody.constraints = RigidbodyConstraints.FreezePositionY;
-			rigidbody.AddForce(difference.normalized * this.magneticStrength);
-		}
+      foreach(GameObject magnet in this.magnets) {
+         Vector3 difference = magnet.transform.position - player.transform.position;
+         difference = new Vector3(difference.x, 0.0f, difference.z);
+         if (difference.magnitude <= this.effectiveRadius) {
+            Rigidbody rigidbody = magnet.GetComponent(typeof(Rigidbody)) as Rigidbody;
+            Vector3 force = difference.normalized * this.magneticStrength;
+            rigidbody.AddForce(force, ForceMode.VelocityChange);
+         }
+      }
 	}
 }
