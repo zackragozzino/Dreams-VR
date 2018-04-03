7b5b48d8c02f98e25489827b32802c69b6974b3d
Zack Ragozzino <zackragozzino@gmail.com>
2018-04-02 23:43:52 -0700

    Fixed collision issue with doors

--- Dreams VR/Assets/Scripts/DoorController.cs
+++ Dreams VR/Assets/Scripts/DoorController.cs
@@ -13,25 +13,21 @@ public class DoorController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-
-		if (Input.GetKeyDown (KeyCode.R))
-			doorAnimation.Play ("Door_open");
-
-		if (Input.GetKeyDown (KeyCode.T))
-			doorAnimation.Play ("Door_Close");
 		
 	}
 
 	void OnTriggerEnter(Collider other){
 		if (other.tag == "Player") {
 			doorAnimation.Play ("Door_open");
+		} else {
+			Destroy (other.gameObject);
 		}
 	}
 
-	void OnTriggerExit(Collider other){
+	/*void OnTriggerExit(Collider other){
 		if (other.tag == "Player") {
 			doorAnimation.Play ("Door_Close");
 		}
-	}
+	}*/
 
 }
