44ef677afb8fc0c9ce25039ba3f3215549390b77
Zack Ragozzino <zackragozzino@gmail.com>
2018-04-03 19:26:49 -0700

    Level loading progress

--- Dreams VR/Assets/Scripts/Director.cs
+++ Dreams VR/Assets/Scripts/Director.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Director : MonoBehaviour {
 
@@ -13,12 +14,17 @@ public class Director : MonoBehaviour {
 	private int timerMax = 30;
 
 	private GameObject player;
+	private GameObject mapGenerator;
+
+	private Scene currentScene; 
 
 	// Use this for initialization
 	void Start () {
 		timer = Random.Range (timerMin, timerMax);
-		player = GameObject.FindGameObjectWithTag ("Player");
-		StartCoroutine (GeneratePortal ());
+
+		SceneManager.sceneLoaded += OnSceneLoaded;
+
+		StartCoroutine (LoadAsynchronously ());
 	}
 	
 	// Update is called once per frame
@@ -33,17 +39,60 @@ public class Director : MonoBehaviour {
 
 		if (Input.GetKeyDown (KeyCode.G)) {
 			Instantiate (dreamScripts [0], player.transform.position, Quaternion.identity, this.transform);
-			Debug.Log ("test");
+		}
+
+		if (Input.GetKeyDown (KeyCode.K)) {
+			//StartCoroutine (LoadAsynchronously ());
+			StartCoroutine(DestroyAndQueueScene());
+
 		}
 		
+	}
+
+	public void GenerateNewWorld(){
+		StartCoroutine(DestroyAndQueueScene());
+	}
+
+	IEnumerator LoadAsynchronously(){
+		AsyncOperation operation = SceneManager.LoadSceneAsync ("Flat_Land", LoadSceneMode.Additive);
+		while (!operation.isDone) {
+			float progress = Mathf.Clamp01 (operation.progress / .9f);
+			Debug.Log ("Loading ... " + progress * 100f + "%");
+			yield return null;
+		}
+		//Reset player and map generator  reference
+		player = GameObject.FindGameObjectWithTag ("Player");
+		mapGenerator = GameObject.FindGameObjectWithTag ("MapGenerator");
+		//Start producing portals now that the scene is loaded
+		StartCoroutine (GeneratePortal ());
+	}
+
+	IEnumerator DestroyAndQueueScene(){
+		StopCoroutine (GeneratePortal ());
+
+		AsyncOperation operation = SceneManager.UnloadSceneAsync (currentScene);
+		while (!operation.isDone) {
+			yield return null;
+		}
+		StartCoroutine (LoadAsynchronously ());
+	}
+
+	void OnSceneLoaded(Scene scene, LoadSceneMode mode){
+		Debug.Log ("Scene loaded: " + scene.name);
+		currentScene = scene;
+		//SceneManager.LoadScene -= OnSceneLoaded;
 	}
 
 	//This code is kind of basic and should be updated to reflect player movement, not time elapsed
 	IEnumerator GeneratePortal(){
 		yield return new WaitForSeconds(doorSpawnRate);
 
-		Vector3 doorPos = new Vector3 (player.transform.position.x + Random.Range (50, 80), doorPortal.transform.position.y, player.transform.position.z + Random.Range (50, 80));
-		GameObject spawnedDoor = Instantiate (doorPortal, doorPos, doorPortal.transform.rotation, this.transform);
+		//Random ranges between -80 to 80 but not within 50 units of the player
+		float xPos = player.transform.position.x + (Random.Range (50, 80) * ((Random.Range (0, 2) == 0) ? 1 : -1));
+		float zPos = player.transform.position.z + (Random.Range (50, 80) * ((Random.Range (0, 2) == 0) ? 1 : -1));
+
+		Vector3 doorPos = new Vector3 (xPos, doorPortal.transform.position.y, zPos);
+		GameObject spawnedDoor = Instantiate (doorPortal, doorPos, doorPortal.transform.rotation, mapGenerator.transform);
 		spawnedDoor.transform.LookAt (player.transform.position);
 		spawnedDoor.transform.rotation = Quaternion.Euler (0, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z);
 
---
+++ Dreams VR/Assets/Scripts/PortalScripts/DoorPortalController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorPortalController : MonoBehaviour {
+
+	private Director director;
+
+	// Use this for initialization
+	void Start () {
+		director = GameObject.FindGameObjectWithTag ("GameController").GetComponent<Director>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (other.tag == "Player") {
+			Debug.Log ("Entering....");
+			director.GenerateNewWorld ();
+		}
+	}
+}
