82c3cfbb96de598818136e2f8795e6a18b0ede30
Paula Ledgerwood <paledger@gmail.com>
2018-04-16 22:00:11 -0700

    added bounce and updated magnetism to attach to a gameobject

--- Dreams VR/Assets/Scripts/PaulaScripts/Bounce.cs
+++ Dreams VR/Assets/Scripts/PaulaScripts/Bounce.cs
@@ -4,9 +4,27 @@ using UnityEngine;
 
 public class Bounce : MonoBehaviour {
 
+   public GameObject item;
+   public PhysicMaterial material;
+   public float bounciness = 0.8f;
+
 	// Use this for initialization
 	void Start () {
-		
+      if (gameObject) {
+         this.item = gameObject;
+      }
+		Collider collider = item.GetComponent(typeof(Collider)) as Collider;
+      if (!collider) {
+         collider = item.AddComponent(typeof(BoxCollider)) as BoxCollider;
+      } 
+      if (!this.material) {
+         this.material = new PhysicMaterial();
+      }
+      // Set correct settings of PhysicMaterial
+      this.material.bounciness = this.bounciness;
+      this.material.bounceCombine = PhysicMaterialCombine.Maximum;
+
+      collider.material = this.material;
 	}
 	
 	// Update is called once per frame
--- Dreams VR/Assets/Scripts/PaulaScripts/Magnetism.cs
+++ Dreams VR/Assets/Scripts/PaulaScripts/Magnetism.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Magnetism : MonoBehaviour {
 
-	public GameObject[] magnets;
+	public GameObject magnet;
 	public GameObject player;
 	public int effectiveRadius = 10;
 	public float magneticStrength = 1;
@@ -14,12 +14,22 @@ public class Magnetism : MonoBehaviour {
 		if (player == null) {
 			player = GameObject.FindGameObjectWithTag ("Player");
 		}
-      magnets = GameObject.FindGameObjectsWithTag("Magnet");
-      foreach (GameObject magnet in magnets) {
-         checkRigidbody(magnet);
-         checkCollider(magnet);
+      if (gameObject) {
+         magnet = gameObject;
+      } 
+      else {
+         magnet = GameObject.FindGameObjectWithTag("Magnet");
       }
+      checkCollider(magnet);
+      checkRigidbody(magnet);
 	}
+
+   void checkCollider(GameObject item){
+      Collider collider = item.GetComponent(typeof(Collider)) as Collider;
+      if (!collider) {
+         collider = item.AddComponent(typeof(BoxCollider)) as BoxCollider;
+      }
+   }
 	
    void checkRigidbody(GameObject item) {
       Rigidbody rigidbody = item.GetComponent(typeof(Rigidbody)) as Rigidbody;
@@ -32,23 +42,14 @@ public class Magnetism : MonoBehaviour {
          RigidbodyConstraints.FreezeRotationZ;
    }
 
-   void checkCollider(GameObject item){
-      Collider collider = item.GetComponent(typeof(Collider)) as Collider;
-      if (!collider) {
-         collider = item.AddComponent(typeof(BoxCollider)) as BoxCollider;
-      }
-   }
-
 	// Update is called once per frame
 	void Update () {
-      foreach(GameObject magnet in this.magnets) {
-         Vector3 difference = magnet.transform.position - player.transform.position;
-         difference = new Vector3(difference.x, 0.0f, difference.z);
-         if (difference.magnitude <= this.effectiveRadius) {
-            Rigidbody rigidbody = magnet.GetComponent(typeof(Rigidbody)) as Rigidbody;
-            Vector3 force = difference.normalized * this.magneticStrength;
-            rigidbody.AddForce(force, ForceMode.VelocityChange);
-         }
+      Vector3 difference = magnet.transform.position - player.transform.position;
+      difference = new Vector3(difference.x, 0.0f, difference.z);
+      if (difference.magnitude <= this.effectiveRadius) {
+         Rigidbody rigidbody = magnet.GetComponent(typeof(Rigidbody)) as Rigidbody;
+         Vector3 force = difference.normalized * this.magneticStrength;
+         rigidbody.AddForce(force, ForceMode.VelocityChange);
       }
 	}
 }
