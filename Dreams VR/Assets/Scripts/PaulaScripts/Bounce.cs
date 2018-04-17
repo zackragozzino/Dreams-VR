4051d9d855321cf3d7031664291b4f54a67f439f
Paula Ledgerwood <paledger@gmail.com>
2018-04-17 15:48:25 -0700

    fixed bounciness

--- Dreams VR/Assets/Scripts/PaulaScripts/Bounce.cs
+++ Dreams VR/Assets/Scripts/PaulaScripts/Bounce.cs
@@ -6,26 +6,49 @@ public class Bounce : MonoBehaviour {
 
    public GameObject item;
    public PhysicMaterial material;
-   public float bounciness = 0.8f;
+   public float bounciness = 0.0f;
 
 	// Use this for initialization
 	void Start () {
       if (gameObject) {
          this.item = gameObject;
       }
-		Collider collider = item.GetComponent(typeof(Collider)) as Collider;
+      Collider collider = getCollider(this.item);
+      addRigidbody(this.item);
+      checkBouncyMaterial();
+      collider.material = this.material;
+	}
+
+   Collider getCollider(GameObject item){
+      Collider collider = item.GetComponent(typeof(Collider)) as Collider;
       if (!collider) {
-         collider = item.AddComponent(typeof(BoxCollider)) as BoxCollider;
-      } 
+         collider = item.AddComponent(typeof(SphereCollider)) as SphereCollider;
+      }
+      return collider;
+   }
+
+   void addRigidbody(GameObject item) {
+      Rigidbody rigidbody = item.GetComponent(typeof(Rigidbody)) as Rigidbody;
+      if (!rigidbody) {
+         rigidbody = item.AddComponent(typeof(Rigidbody)) as Rigidbody;
+      }
+      // rigidbody.constraints = RigidbodyConstraints.FreezePositionY | 
+      //    RigidbodyConstraints.FreezeRotationX |
+      //    RigidbodyConstraints.FreezeRotationY | 
+      //    RigidbodyConstraints.FreezeRotationZ;
+   }   
+   void checkBouncyMaterial() {
+
       if (!this.material) {
          this.material = new PhysicMaterial();
       }
       // Set correct settings of PhysicMaterial
+      if (this.bounciness == 0) {
+         this.bounciness = Random.RandomRange(0.5f, 1.0f);
+      }
       this.material.bounciness = this.bounciness;
       this.material.bounceCombine = PhysicMaterialCombine.Maximum;
-
-      collider.material = this.material;
-	}
+   }
 	
 	// Update is called once per frame
 	void Update () {
