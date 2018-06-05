37dab172dec642b03e09d9a0029ec84d03f60617
Paula Ledgerwood <paledger@gmail.com>
2018-06-05 11:05:13 -0700

    added new splashscreen

---
+++ Dreams VR/Assets/Scripts/PaulaScripts/SplashScreen.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SplashScreen : MonoBehaviour {
+
+   public RawImage logo;
+   public RawImage backSplash;
+
+	// Use this for initialization
+	void Start () {		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
